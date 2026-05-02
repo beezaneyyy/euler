2105d02fb7f265347c52da5c8fe3e8b312eacdb5
Beezaneyyy <matheusbiz070207@gmail.com>
2026-05-02 15:25:28 -0300

    ex20

---
+++ ex20/ex20.py
@@ -0,0 +1,14 @@
+resultado = 1
+soma = 0
+
+for i in range(100, 1, -1):
+    resultado *= i
+
+stresultado = str(resultado)
+for n in stresultado:
+    numero = int(n)
+    soma += numero
+
+print(soma)
+
+#Soma todos os dígitos de 100!
